b3ac66c4335cfc5623454c81e71cb1f08b9420b0
Sanath <sanath@serrexlabs.com>
2022-05-29 01:31:59 +0530

    init project

---
+++ src/utils/bin/sumfetch.ts
@@ -0,0 +1,83 @@
+import config from '../../../config.json';
+
+const sumfetch = async (args: string[]): Promise<string> => {
+  if (config.ascii === 'sanath') {
+    return `
+__________________¶¶¶____¶¶¶____1¶¶1______________         sumfetch: summary display
+___________________¶¶¶____¶¶¶____¶¶¶______________        -----------
+___________________¶¶¶____¶¶¶____¶¶¶______________         ABOUT
+__________________¶¶¶____1¶¶1___1¶¶1______________         ${config.name}
+________________1¶¶¶____¶¶¶____¶¶¶1_______________         <u><a href="${config.resume_url}" target="_blank">My resumè</a></u>
+______________1¶¶¶____¶¶¶1___¶¶¶1_________________        爵 <u><a href="${config.blog_url}" target="_blank">blog</a></u>
+_____________¶¶¶1___1¶¶1___1¶¶1___________________
+____________1¶¶1___1¶¶1___1¶¶1____________________        -----------
+____________1¶¶1___1¶¶1___1¶¶¶____________________         CONTACT 
+_____________¶¶¶____¶¶¶1___¶¶¶1___________________         <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+______________¶¶¶¶___1¶¶¶___1¶¶¶__________________         <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+_______________1¶¶¶1___¶¶¶1___¶¶¶¶________________         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+_________________1¶¶1____¶¶¶____¶¶¶_______________        ☎ <u><a href="tel:${config.phone}" target="_blank">${config.phone}</a></u>
+___________________¶¶1____¶¶1____¶¶1______________        
+___________________¶¶¶____¶¶¶____¶¶¶______________        -----------        
+__________________1¶¶1___1¶¶1____¶¶1______________        💀 SKILLS 
+_________________¶¶¶____¶¶¶1___1¶¶1_______________        ⚡ Typescript, Javascript, Golang, Dart, PHP
+________________11_____111_____11_________________        ☕ Nodejs, ReactJs, ReactNative, Flutter
+__________¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶________        ☁ GraphQL, NoSQL
+1¶¶¶¶¶¶¶¶¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶________
+1¶¶¶¶¶¶¶¶¶¶¶__1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶________         -----------
+1¶¶_______¶¶__1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶________        ☞ SUMMARY
+1¶¶_______¶¶__1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶________        I have been doing software engineering for the last ten years.
+1¶¶_______¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶________        My specialization is in Backend technologies such as NodeJs 
+1¶¶_______¶¶__1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶________        with Typescript, GraphQL, PHP, NoSQL, and AWS. But my abilities
+_¶¶¶¶¶¶¶¶¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶________        are not limited to the backend. Also, I have good experience in 
+_¶¶¶¶¶¶¶¶¶¶¶__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶________        technologies such as React, Flutter, and ReactNative.I have been
+__________¶¶___1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶1________        helping companies grow their product to support millions of users
+__________1¶¶___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_________        throughout my career. I’m a fast learner.I like to accept any
+____________¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶11__________        challenges coming my way; that drive me in my career. 
+11_____________________________________________111        I believe that is my true strength.
+1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶1
+__¶¶111111111¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶111111111¶__
+`;
+  }
+  if (config.ascii === 'cveinnt') {
+    return `                                                  
+             @@@@@@@@@@@@@                   sumfetch: summary display
+        @@@@               @@@@             -----------
+      @@                       @@            ABOUT
+    @@                           @@          ${config.name}
+                                   @@       ﰩ ${config.ps1_hostname}
+ @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+@@                                   @@     -----------
+@@             .@@@@@@@@@@.          @@      CONTACT 
+ @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+     @@@                        @@@         -----------
+        @@@                  @@@ @@         
+         @|  @@@@@@@@@@@@@@@@   @@          
+         @|                      @@         
+
+`;
+  } else {
+    return `
+           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  sumfetch
+        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
+      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+▐▓                                 ▐▓       -----------
+▐▓        > L I V E T E R M        ▐▓        CONTACT 
+▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             DONATE 
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
+
+`;
+  }
+};
+
+export default sumfetch;
